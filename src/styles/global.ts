3dcc99d73974ddda26263b95ea1a28afb9149ad7
Marcos Andrade <marcos.muniz@b2wdigital.com>
2022-03-02 12:09:00 -0300

    install fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,13 +5,13 @@ export const GlobalStyle = createGlobalStyle`
         --red: #E52E4D;
         --blue:#5429CC
         
-        --blue-light: #6933FF
+        --blue-light: #6933FF;
         
-        --text-title: #363F5F
-        --text-body: #969CB3
+        --text-title: #363F5F;
+        --text-body: #969CB3;
         
         --background: #f0f2f5;
-        --shape: #FFFFFF
+        --shape: #FFFFFF;
     }
     
     * {
@@ -33,6 +33,15 @@ export const GlobalStyle = createGlobalStyle`
     body { 
         background: var(--background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
     }
 
     button: {
