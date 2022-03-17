707dfc5212decd8058c5100fc18d7b7e73679cd7
Marcos Andrade <marcos.muniz@b2wdigital.com>
2022-03-17 16:00:56 -0300

    estilização radio box in-out com styled comp props

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form`
     h2 {
@@ -55,30 +55,47 @@ export const TransactionTypeContainer = styled.div`
     gap: 0.5rem;
 
     button {
-        height: 4rem;
-        border: 1px solid #d7d7d7;
-        border-radius: 0.25rem;
+    }
+`;
 
-        background: transparent;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        transition: hover 0.2s;
+interface RadioBoxProps {
+    isActive: boolean;
+    activeColor: 'green' | 'red';
+}
 
-        &:hover {
-            border-color: ${darken(0.1, '#d7d7d7')};
-        }
+const colors = {
+    green: '#33cc95',
+    red: '#e52e4d'
+};
 
-        img {
-            width: 20px;
-            height: 20px;
-        }
+export const RadioBox = styled.button<RadioBoxProps>`
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
 
-        span {
-            display: inline-block;
-            margin-left: 1rem;
-            font-size: 1rem;
-            color: var(--text-title);
-        }
+    background: ${props =>
+        props.isActive
+            ? transparentize(0.9, colors[props.activeColor])
+            : 'transparent'};
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: hover 0.2s;
+
+    &:hover {
+        border-color: ${darken(0.1, '#d7d7d7')};
+    }
+
+    img {
+        width: 20px;
+        height: 20px;
+    }
+
+    span {
+        display: inline-block;
+        margin-left: 1rem;
+        font-size: 1rem;
+        color: var(--text-title);
     }
 `;
