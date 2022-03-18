d6fda98edbde2501dea908554206d3bbafe8695a
Marcos Andrade <marcos.muniz@b2wdigital.com>
2022-03-18 09:09:57 -0300

    configuração table dinamica

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
         transaction: Model
     },
 
+    seeds(server) {
+        server.db.loadData({
+            transactions: [
+                {
+                    id: 1,
+                    title: 'Desenvolvimento de Website',
+                    type: 'deposit',
+                    category: 'Dev',
+                    amount: 6000,
+                    createdAt: new Date('2021-02-20 09:00:00')
+                },
+                {
+                    id: 2,
+                    title: 'Aluguel',
+                    type: 'withdraw',
+                    category: 'Casa',
+                    amount: 1100,
+                    createdAt: new Date('2021-02-05 09:00:00')
+                }
+            ]
+        });
+    },
+
     routes() {
         this.namespace = 'api';
 
