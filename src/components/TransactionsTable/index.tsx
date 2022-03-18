bec7f3ab0299a06d3ce965a0fc22edb85046b44d
Marcos Andrade <marcos.muniz@b2wdigital.com>
2022-03-18 09:33:17 -0300

    create transaction context

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,24 +1,10 @@
-import { useEffect, useState } from 'react';
-import { api } from '../../services/api';
-import { Container } from './styles';
+import { useContext } from 'react';
 
-interface Transaction {
-    id: number;
-    title: string;
-    amount: number;
-    type: string;
-    category: string;
-    createdAt: string;
-}
+import { Container } from './styles';
+import { TransactionsContext } from '../../TransactionsContext';
 
 export function TransactionsTable() {
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-    useEffect(() => {
-        api.get('transactions').then(response =>
-            setTransactions(response.data.transactions)
-        );
-    }, []);
+    const transactions = useContext(TransactionsContext);
 
     return (
         <Container>
