14d83300b12986f12b7b59348f77c7ac6f1e87dc
Marcos Andrade <marcos.muniz@b2wdigital.com>
2022-03-19 10:17:42 -0300

    create transaction by context

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,11 +1,14 @@
-import { useState, FormEvent } from 'react';
+import { useState, FormEvent, useContext } from 'react';
 import Modal from 'react-modal';
+
+import { api } from '../../services/api';
+import { TransactionsContext } from '../../TransactionsContext';
+
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-
 import closeImg from '../../assets/close.svg';
+
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
-import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -16,23 +19,22 @@ export function NewTransactionModal({
     isOpen,
     onRequestClose
 }: NewTransactionModalProps) {
+    const { createTransaction } = useContext(TransactionsContext);
+
     const [title, setTitle] = useState('');
-    const [value, setValue] = useState(0);
+    const [amount, setAmount] = useState(0);
     const [category, setCategory] = useState('');
 
     const [type, setType] = useState('deposit');
 
     function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
-
-        const transactionData = {
+        createTransaction({
             title,
-            value,
-            category,
-            type
-        };
-
-        api.post('/transactions', transactionData);
+            amount,
+            type,
+            category
+        });
     }
 
     return (
@@ -61,8 +63,8 @@ export function NewTransactionModal({
                 <input
                     type="number"
                     placeholder="Valor"
-                    value={value}
-                    onChange={event => setValue(Number(event.target.value))}
+                    value={amount}
+                    onChange={event => setAmount(Number(event.target.value))}
                 />
 
                 <TransactionTypeContainer>
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,8 +6,8 @@ import totalImg from '../../assets/total.svg';
 import { TransactionsContext } from '../../TransactionsContext';
 
 export function Summary() {
-    const data = useContext(TransactionsContext);
-    console.log(data);
+    const { transactions } = useContext(TransactionsContext);
+    console.log(transactions);
 
     return (
         <Container>
